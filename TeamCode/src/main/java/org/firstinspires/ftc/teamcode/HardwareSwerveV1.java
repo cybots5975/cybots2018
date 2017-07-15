93f539d398029cd5ef40f16e3f0ff27964c4ddee
Karter Krueger <krueger.karter@gmail.com>
2017-07-14 23:38:25 -0500

    Added IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSwerveV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSwerveV1.java
@@ -1,10 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorBNO055IMU;
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorKLNavxMicro;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 /**
  * This is NOT an opmode.
@@ -44,6 +52,12 @@ public class HardwareSwerveV1
     public AnalogInput  DSensor2 = null; //Driver Sensor Back (2)
     public AnalogInput  PSensor1 = null; //Passenger Sensor Front (1)
     public AnalogInput  PSensor2 = null; //Passenger Sensor Back (2)
+
+    public BNO055IMU imu;
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
 
     /* Local OpMode members. */
     HardwareMap hwMap  = null;
@@ -88,6 +102,22 @@ public class HardwareSwerveV1
         DServo2.setPosition(0); //Set Driver Servo Back(2) to 0 power
         PServo1.setPosition(0); //Set Pass Servo Front(1) to 0 power
         PServo2.setPosition(0); //Set Pass Servo Back(2) to 0 power
+
+
+//IMU initialization
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
     }
 
