be9e30fd7413710bce89d1a0d0c68692db58fd9a
Karter Krueger <krueger.karter@gmail.com>
2017-11-18 00:24:48 -0500

    Added programs for automatically transitioning auto and testing LED strip

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general/AutoTransitioningAuto.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+
+@Autonomous(name = "AutoTransitioningAuto")
+public class AutoTransitioningAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Initializing Here", true);
+        telemetry.update();
+
+
+        AutoTransitioner.transitionOnStop(this, "Robot Teleop");
+        // AutoTransitioner used before waitForStart()
+        waitForStart();
+
+
+        telemetry.addData("Timer", new Func<Double>() {
+            @Override
+            public Double value() {
+                return getRuntime();
+            }
+        });
+        while (opModeIsActive()) {
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general/VuforiaToOpenCV.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.general;
+
+/**
+ * Created by kskrueger on 11/15/17.
+ */
+
+public class VuforiaToOpenCV {
+}
