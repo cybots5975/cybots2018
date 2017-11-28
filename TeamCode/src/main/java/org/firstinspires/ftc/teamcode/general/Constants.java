4af208e6143a7726426d6f6017c4af33259e22ec
Karter Krueger <krueger.karter@gmail.com>
2017-11-27 21:39:55 -0600

    Adjust line spacing for minor tweak

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general/Constants.java
@@ -13,6 +13,7 @@ public class Constants {
     private static final double     COUNTS_PER_MOTOR_REV    = 1440;    // eg: TETRIX Motor Encoder
     private static final double     DRIVE_GEAR_REDUCTION    = 2/3;     // This is < 1.0 if geared UP
     private static final double     WHEEL_DIAMETER_INCHES   = 4.0;     // For figuring circumference
-    public static final double     COUNTS_PER_INCH          = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
+    public static final double      COUNTS_PER_INCH          = (COUNTS_PER_MOTOR_REV *
+                                                                DRIVE_GEAR_REDUCTION) /
+                                                                (WHEEL_DIAMETER_INCHES * Math.PI);
 }
