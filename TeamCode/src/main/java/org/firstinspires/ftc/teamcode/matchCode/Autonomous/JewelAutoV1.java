35e9c9601426849226f99038fba2638ed9fec734
Karter Krueger <krueger.karter@gmail.com>
2017-12-18 23:25:00 -0600

    Auto minor tweaks and testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/matchCode/Autonomous/JewelAutoV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/matchCode/Autonomous/JewelAutoV1.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.corningrobotics.enderbots.endercv.CameraViewDisplay;
-import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.teamcode.general.AutoTransitioner;
 import org.firstinspires.ftc.teamcode.general.Robot;
 import org.firstinspires.ftc.teamcode.test.ExampleBlueVision;
@@ -17,14 +16,12 @@ import static org.firstinspires.ftc.teamcode.test.ExampleBlueVision.order.unknow
  * Created by kskrueger on 10/22/17.
  */
 
-@Autonomous(name="Test Glyph Auto RED", group="Swerve")
-public class GlyphAutoV1_opp extends LinearOpMode{
+@Autonomous(name="Jewel Auto Trial", group="AutoOnly")
+public class JewelAutoV1 extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
 
     private ExampleBlueVision blueVision;
 
-    private RelicRecoveryVuMark VuMark;
-    private double driveTime, distance;
     private double kickCenter = .45, raisedArm = .02;
     private boolean loop = true;
 
@@ -33,29 +30,14 @@ public class GlyphAutoV1_opp extends LinearOpMode{
 
     @Override
     public void runOpMode() {
-        robot.Vuforia = true;
         robot.init(hardwareMap);
-
-        gamepad1.setJoystickDeadzone(.01F); //Set joystick deadzone to a lower number
-
-        robot.VuMark1.activate();
 
         while (!isStarted()&&!isStopRequested()) {
             robot.JewelArm.setPosition(raisedArm);
             robot.JewelKick.setPosition(kickCenter);
-            robot.drive.setEfficiency(false);
-            robot.drive.holdModuleAngle(90+180);
-            telemetry.addData("VuMark",robot.VuMark1.scan().toString());
-            if (robot.VuMark1.scan().equals(RelicRecoveryVuMark.UNKNOWN)) {
-                VuMark = RelicRecoveryVuMark.CENTER;
-            } else {
-                VuMark = robot.VuMark1.scan();
-            }
             telemetry.addData("Status", "Initialized");
             telemetry.update();
         }
-
-        robot.VuMark1.close();
         AutoTransitioner.transitionOnStop(this, "Teleop V1");
         waitForStart();
         if (isStarted()) {
@@ -72,7 +54,7 @@ public class GlyphAutoV1_opp extends LinearOpMode{
 
                 blueVision.setShowBlue(true);
 
-                pause(4,false);
+                sleep(4000);
 
                 //jewelsOrder = jewelsOrder.blueFirst;
                 switch (jewelsOrder) {
@@ -80,7 +62,6 @@ public class GlyphAutoV1_opp extends LinearOpMode{
                         runtime.reset();
                         while (runtime.seconds() < 2) {
                             robot.JewelKick.setPosition(0);
-                            robot.drive.holdModuleAngle(90+180);
                             telemetry.addData("Red jewel","stop");
                             telemetry.update();
                         }
@@ -91,7 +72,6 @@ public class GlyphAutoV1_opp extends LinearOpMode{
                         runtime.reset();
                         while (runtime.seconds() < 2) {
                             robot.JewelKick.setPosition(1);
-                            robot.drive.holdModuleAngle(90+180);
                             telemetry.addData("Blue jewel","stop");
                             telemetry.update();
                         }
@@ -100,76 +80,13 @@ public class GlyphAutoV1_opp extends LinearOpMode{
                         break;
                 }
 
-                robot.drive.setEfficiency(false);
-                pause(1.5, false);
-
-                switch (VuMark) {
-                    case LEFT:
-                        driveTime = 2;
-                        distance = 24;
-                        break;
-                    case CENTER:
-                        driveTime = 2.5;
-                        distance = 24 + 6;
-                        break;
-                    case RIGHT:
-                        driveTime = 3;
-                        distance = 24 + 6 + 6;
-                        break;
-                }
-
-                runtime.reset();
-                while (runtime.seconds() < driveTime) {
-                    robot.drive.RobotCentric(.15, 0, 0, false);
-                }
-
-                pause(3, true);
-
-                robot.intake.setSpeed(-.5);
-
-                pause(2,true);
-
-                robot.drive.setEfficiency(true);
-                runtime.reset();
-                while (runtime.seconds() < 1) {
-                    robot.drive.RobotCentric(0, .2, 0, true);
-                }
-
-                pause(2, true);
-
-                robot.drive.setEfficiency(true);
-
-                runtime.reset();
-                while (runtime.seconds() < 1) {
-                    robot.drive.RobotCentric(0, -.15, 0, true);
-                }
-
-                robot.intake.setSpeed(0);
+                sleep(3000);
 
                 blueVision.setShowBlue(false);
                 blueVision.disable();
 
                 loop = false;
             }
-        }
-    }
-
-    public void pause(double seconds, boolean fwd) {
-        runtime.reset();
-        while (runtime.seconds()<seconds){
-            if (fwd) {
-                robot.drive.RobotCentric(0,.001,0,true);
-                //robot.drive.setEfficiency(false);
-                //robot.drive.holdModuleAngle(0);
-            } else {
-                robot.drive.RobotCentric(.001,0,0,false);
-                //robot.drive.setEfficiency(false);
-                //robot.drive.holdModuleAngle(90+180);
-            }
-            //wait
-            telemetry.addData("Waiting",seconds+" seconds");
-            telemetry.addData("Time",runtime.seconds());
-            telemetry.update();
         }
     }
 }
