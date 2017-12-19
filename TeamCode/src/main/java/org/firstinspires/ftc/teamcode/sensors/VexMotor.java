e717d9e72a1379e970ed6f5a888638aff1691d05
Karter Krueger <krueger.karter@gmail.com>
2017-12-18 23:24:44 -0600

    Vex motor function in the works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/VexMotor.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.sensors;
+
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+
+/**
+ * Created by kskrueger on 12/16/17.
+ */
+
+public class VexMotor {
+
+    public VexMotor(CRServoImplEx vexMotor, MA3Encoder encoder) {
+
+    }
+}
