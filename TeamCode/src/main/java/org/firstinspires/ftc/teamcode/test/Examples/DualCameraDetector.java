fceb589f17f368bf2c39c08d5e6fa02f771a9ce7
Karter Krueger <krueger.karter@gmail.com>
2017-12-25 16:31:09 -0600

    Attempted dual camera detector....FAIL

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/Examples/DualCameraDetector.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.test.Examples;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.detectors.GlyphDetector;
+import com.disnodeteam.dogecv.detectors.JewelDetector;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by kskrueger for Cybots Robotics on 12/23/17.
+ */
+
+@TeleOp(name="DogeCV Dual Detector", group="DogeCV")
+public class DualCameraDetector extends OpMode{
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private JewelDetector jewelDetector = null;
+    private GlyphDetector glyphDetector = null;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        jewelDetector = new JewelDetector();
+        jewelDetector.init(hardwareMap.appContext, CameraViewDisplay.getInstance(),1);
+
+        //Jewel Detector Settings
+        jewelDetector.areaWeight = 0.02;
+        jewelDetector.detectionMode = JewelDetector.JewelDetectionMode.MAX_AREA; // PERFECT_AREA
+        //jewelDetector.perfectArea = 6500; <- Needed for PERFECT_AREA
+        jewelDetector.debugContours = true;
+        jewelDetector.maxDiffrence = 15;
+        jewelDetector.ratioWeight = 15;
+        jewelDetector.minArea = 700;
+
+        jewelDetector.enable();
+
+        glyphDetector = new GlyphDetector();
+        glyphDetector.init(hardwareMap.appContext, CameraViewDisplay.getInstance(),0);
+        glyphDetector.minScore = 1;
+        glyphDetector.downScaleFactor = 0.3;
+        glyphDetector.speed = GlyphDetector.GlyphDetectionSpeed.SLOW;
+        glyphDetector.enable();
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+        telemetry.addData("Current Order", "Jewel Order: " + jewelDetector.getCurrentOrder().toString()); // Current Result
+        telemetry.addData("Last Order", "Jewel Order: " + jewelDetector.getLastOrder().toString()); // Last Known Result
+
+        telemetry.addData("Glyph Pos X", glyphDetector.getChosenGlyphOffset());
+        telemetry.addData("Glyph Pos Offest", glyphDetector.getChosenGlyphPosition().toString());
+    }
+
+    @Override
+    public void stop() {
+        jewelDetector.disable();
+        glyphDetector.disable();
+    }
+}
