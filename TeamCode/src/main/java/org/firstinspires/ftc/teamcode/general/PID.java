0726d3f2468020024f46cb81c79a10b9a7938350
Karter Krueger <krueger.karter@gmail.com>
2017-12-26 17:26:44 -0600

    put PID in it's own class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general/PID.java
@@ -1,9 +1,47 @@
 package org.firstinspires.ftc.teamcode.general;
 
 /**
- * Created by kskrueger on 10/14/17.
+ * Created by kskrueger on 12/25/17.
  */
 
 public class PID {
+    private double kP;
+    private double kI;
+    private double kD;
+    private double lastTime;
+    private int integral = 0;
+    private int previousError = 0;
 
+    public PID(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+    }
+
+    public PID(){
+    }
+
+    public void setVariables(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+    }
+
+    public double run(int targetValue, int position) {
+        double dt = (System.currentTimeMillis() - lastTime);
+        lastTime = System.currentTimeMillis();
+
+        int angleError = (targetValue - position);
+        angleError -= (360*Math.floor(0.5+(((double) angleError)/360.0)));
+
+        int error = angleError;
+
+        integral += kI * error * dt;
+
+        double u = (kP * error + integral + kD * (error - previousError) / dt);
+
+        previousError = error;
+
+        return u;
+    }
 }
